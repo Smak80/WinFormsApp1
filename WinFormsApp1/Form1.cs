1915037ae86f26b9a2aa40b3c8d46f143036c528
Smak80 <smaklets@kpfu.ru>
2021-03-19 11:38:00 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Painter p;
+        private bool buttonPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            p = new Painter(panel1.Size);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            buttonPressed = true;
+            p.StartPoint = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (buttonPressed)
+            {
+                p.EndPoint = e.Location;
+                p.Preview(panel1.CreateGraphics());
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            buttonPressed = false;
+            p.EndPoint = e.Location;
+            p.Paint();
+            panel1.Update();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            p.Show(panel1.CreateGraphics());
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            p.ImageSize = panel1.Size;
+        }
+    }
+}
---
+++ WinFormsApp1/Painter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    class Painter
+    {
+        private Image mainImg;
+        private Size imageSize;
+        public Size ImageSize
+        {
+            get => imageSize;
+            set
+            {
+                imageSize = value;
+                var img = new Bitmap(imageSize.Width, imageSize.Height);
+                var ig = Graphics.FromImage(img);
+                ig.Clear(Color.White);
+                if (mainImg != null)
+                {
+                    ig.DrawImage(mainImg, 0, 0);
+                }
+
+                mainImg = img;
+            }
+        }
+        public Point StartPoint = new Point(0, 0);
+        public Point EndPoint = new Point(0, 0);
+
+        public Painter(Size sz)
+        {
+            ImageSize = sz;
+        }
+
+        private void Paint(Graphics g)
+        {
+            Pen p = new Pen(Color.BlueViolet, 5);
+            g.DrawRectangle(p,
+                StartPoint.X,
+                StartPoint.Y,
+                EndPoint.X - StartPoint.X,
+                EndPoint.Y - StartPoint.Y);
+        }
+
+        public void Paint()
+        {
+            var g = Graphics.FromImage(mainImg);
+            Paint(g);
+        }
+
+        public void Preview(Graphics g)
+        {
+            BufferedGraphics bg = BufferedGraphicsManager.Current.Allocate(g, Rectangle.Round(g.VisibleClipBounds));
+            bg.Graphics.DrawImage(mainImg, 0, 0);
+            Paint(bg.Graphics);
+            bg.Render(g);
+        }
+
+        public void Show(Graphics g)
+        {
+            g.DrawImage(mainImg, 0, 0);
+        }
+
+    }
+}
